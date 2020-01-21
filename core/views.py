815eb7e3614aa5ccc9ad00db6e150342a498f94f
Fabien Lenoir <flenoir@gmail.com>
2020-01-21 16:13:09 +0100

    added check on isbn or text as input and loop on result

--- core/views.py
+++ core/views.py
@@ -1,27 +1,30 @@
 from django.shortcuts import render
-# from django.views.generic import TemplateView, FormView
 from .models import Book
 from django.urls import reverse_lazy
 from .form import BookForm, SearchForm
 
-# from isbnlib import meta
-# from isbnlib.registry import bibformatters
-from isbnlib import canonical, meta, cover, desc
-from isbntools.app import *
-
 import json
-from urllib.request import urlopen
-
 import requests
 
 
-def isbn_search(isbn):
+def isbn_text_search(isbn):
     # Voir si pertinent de resoumettre la requette sur le titrre pour avoir la cover
-    r = requests.get('https://www.googleapis.com/books/v1/volumes?q=isbn:'+ isbn)
+    r = requests.get('https://www.googleapis.com/books/v1/volumes?q='+ isbn)
     print(r.text)
     parsed = json.loads(r.text)
     # print(parsed['items'][0]['volumeInfo']['title'])
-    return parsed['items'][0]['volumeInfo']
+    return parsed['items']
+
+def input_cleaner(search_data):
+    try:
+        if type(int(search_data)):
+            res = requests.get('https://www.googleapis.com/books/v1/volumes?q=isbn:'+ str(search_data))
+            parsed_res = json.loads(res.text)
+            return parsed_res['items'][0]['volumeInfo']['title']
+    except ValueError as error:
+        print(error)
+        return search_data
+
 
 def main(request):
     if request.method == "POST":
@@ -32,7 +35,10 @@ def main(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             data = form.cleaned_data["post"].casefold()
-            result = isbn_search(str(data))
+            # check if input is isbn number or title
+            checked_input = input_cleaner(str(data))
+            # search on title
+            result = isbn_text_search(str(checked_input))
             context = {"form": form, "result": result}
             return render(request, "main.html", context)
         return render(request, "main.html", {"form": form})
