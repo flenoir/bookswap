92477fcdf9fae61de944339c3fcb836eefa823c3
Fabien Lenoir <flenoir@gmail.com>
2020-02-06 17:34:56 +0100

    started to add test

---
+++ search/testsrun/test_forms.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from search.form import SearchForm, InviteForm
+
+class FormTests(TestCase):
+    # test search form is valid
+    def test_search_form_is_valid(self):
+        form_data = {"post": "un sac de billes"}
+        form = SearchForm(form_data)
+        self.assertTrue(form.is_valid())
+    
+    # test invite form is valid
+    def test_invite_form_is_valid(self):
+        form_data = {"post": "foo@bar.com"}
+        form = InviteForm(form_data)
+        self.assertTrue(form.is_valid())
+
