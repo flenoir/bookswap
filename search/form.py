62b25e052f42a5c23e364aabc63f245ac3d58928
Fabien Lenoir <flenoir@gmail.com>
2020-02-12 12:07:37 +0100

    enhanced search form modal

--- search/form.py
+++ search/form.py
@@ -8,7 +8,7 @@ class BookForm(forms.ModelForm):
         fields = ('isbn', 'title', 'author', 'availability', 'category', 'cover', 'description' , 'state')
 
 class SearchForm(forms.Form):
-    post = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'ISBN, Title or author', 'size': '100'}))
+    post = forms.CharField(label='', max_length=100, widget=forms.TextInput(attrs={'placeholder': 'entrez ici le code ISBN, le titre ou l\'auteur', 'size': '100'}))
 
 class InviteForm(forms.Form):
     post = forms.CharField(label='', required=False, max_length=50, widget=forms.TextInput(attrs={'placeholder': 'Email', 'size': '40'}) )
