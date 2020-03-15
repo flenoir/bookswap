9fd9bc2bbcdb6e4e2cc16f3ab603eadd4497f28a
Fabien LENOIR <fabien.lenoir@videomenthe.fr>
2020-03-15 15:47:21 +0100

    added boostrap to detail template

--- search/form.py
+++ search/form.py
@@ -17,39 +17,39 @@ from .models import Book
 
 class BookForm(forms.ModelForm):
 
-    # Supercharge form initialization
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.layout = Layout(
-            Div(                
-                Field("title", css_class="w-full"),
-                Div(
-                    Field("author", css_class="my-1 px-1 w-1/2 overflow-hidden"),
-                    Field("publisher", css_class="my-1 px-1 w-1/2 overflow-hidden"),
-                ),
-                Field("description", css_class="w-full"),
-                Div(
-                    Field("isbn", css_class="my-1 px-1 w-1/2 overflow-hidden"),
-                    Field("cover", css_class="my-1 px-1 w-1/2 overflow-hidden"),
-                    # HTML('<img src="{{ "cover" }}"/>'),
-                ),
-                Div(
-                    Field("category", css_class="my-1 px-1 w-1/3 overflow-hidden"),
-                    Field("availability", css_class="my-1 px-1 w-1/3 overflow-hidden"),
-                    Field("state", css_class="my-1 px-1 w-1/3 overflow-hidden"),
-                ),
-                Div(
-                    ButtonHolder(
-                        Submit(
-                            "submit",
-                            "Update",
-                            css_class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
-                        ),
-                    ),
-                ),                
-            )
-        )
+    # # Supercharge form initialization
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.helper = FormHelper()
+    #     self.helper.layout = Layout(
+    #         Div(                
+    #             Field("title", css_class="w-full"),
+    #             Div(
+    #                 Field("author", css_class="my-1 px-1 w-1/2 overflow-hidden"),
+    #                 Field("publisher", css_class="my-1 px-1 w-1/2 overflow-hidden"),
+    #             ),
+    #             Field("description", css_class="w-full"),
+    #             Div(
+    #                 Field("isbn", css_class="my-1 px-1 w-1/2 overflow-hidden"),
+    #                 Field("cover", css_class="my-1 px-1 w-1/2 overflow-hidden"),
+    #                 # HTML('<img src="{{ "cover" }}"/>'),
+    #             ),
+    #             Div(
+    #                 Field("category", css_class="my-1 px-1 w-1/3 overflow-hidden"),
+    #                 Field("availability", css_class="my-1 px-1 w-1/3 overflow-hidden"),
+    #                 Field("state", css_class="my-1 px-1 w-1/3 overflow-hidden"),
+    #             ),
+    #             Div(
+    #                 ButtonHolder(
+    #                     Submit(
+    #                         "submit",
+    #                         "Update",
+    #                         css_class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded",
+    #                     ),
+    #                 ),
+    #             ),                
+    #         )
+    #     )
 
     class Meta:
         model = Book
