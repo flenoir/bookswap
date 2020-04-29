d27ff7dc0aeea0f25059a8a8c93981864d9329ba
Fabien LENOIR <fabien.lenoir@videomenthe.fr>
2020-04-29 12:26:08 +0200

    add postgres ti ci_cd

--- bookswap_project/settings/ci_cd.py
+++ bookswap_project/settings/ci_cd.py
@@ -18,6 +18,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
