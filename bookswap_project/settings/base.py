7f6917c5b634477bf604ff3c89e5f3e1f39c6b61
Fabien LENOIR <fabien.lenoir@videomenthe.fr>
2020-05-01 14:45:22 +0200

    add fontawesome for login and book_list page

--- bookswap_project/settings/base.py
+++ bookswap_project/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'fontawesome_5',
     'crispy_forms',
     'search',
     'users',
