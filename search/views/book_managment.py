790cbf475d87ac273d70b907b5a9bdc81e0167dd
Fabien Lenoir <flenoir@gmail.com>
2020-11-28 17:29:35 +0100

    fix ImageLink error close #58

--- search/views/book_managment.py
+++ search/views/book_managment.py
@@ -11,7 +11,6 @@ def save_book(request, isbn):
     """
     if request.method == "POST":
         data = request.session.get("temp_json")
-
         for i in data:
             if i["volumeInfo"]["industryIdentifiers"][0]["identifier"] == isbn:
                 print("found match !")
@@ -22,41 +21,79 @@ def save_book(request, isbn):
                     i["volumeInfo"]["industryIdentifiers"][0]["identifier"]
                     not in user_library
                 ):
-                    # save book
-                    try:
-                        book = Book.objects.create(isbn=isbn)
-                        book.isbn = i["volumeInfo"]["industryIdentifiers"][0][
-                            "identifier"
-                        ]
-                        book.title = i["volumeInfo"]["title"]
-                        book.author = i["volumeInfo"].get("authors", "...")
-                        book.cover = i["volumeInfo"]["imageLinks"]["thumbnail"]
-                        book.publisher = i["volumeInfo"].get("publisher", "...")
-                        book.description = i["volumeInfo"].get("description", "...")
-                        book.category = i["volumeInfo"].get(
-                            "categories", "uncategorized"
-                        )
-                        book.page_count = i["volumeInfo"].get("pageCount", 0)
-                        book.state = "good condition"
-                        book.availability = True
-                        cured_date = dateparser.parse(i["volumeInfo"]["publishedDate"])
-                        book.published_date = cured_date.date()
-                        book_status = Ownership(
-                            book=book,
-                            customuser=request.user,
-                            state="Neuf",
-                            availability=True,
-                        )
+                    if "imageLinks" in i["volumeInfo"]:
+                        # save book
+                        try:
+                            book = Book.objects.create(isbn=isbn)
+                            book.isbn = i["volumeInfo"]["industryIdentifiers"][0][
+                                "identifier"
+                            ]
+                            book.title = i["volumeInfo"]["title"]
+                            book.author = i["volumeInfo"].get("authors", "...")
+                            book.cover = i["volumeInfo"]["imageLinks"]["thumbnail"]
+                            book.publisher = i["volumeInfo"].get("publisher", "...")
+                            book.description = i["volumeInfo"].get("description", "...")
+                            book.category = i["volumeInfo"].get(
+                                "categories", "uncategorized"
+                            )
+                            book.page_count = i["volumeInfo"].get("pageCount", 0)
+                            book.state = "good condition"
+                            book.availability = True
+                            cured_date = dateparser.parse(i["volumeInfo"]["publishedDate"])
+                            book.published_date = cured_date.date()
+                            book_status = Ownership(
+                                book=book,
+                                customuser=request.user,
+                                state="Neuf",
+                                availability=True,
+                            )
 
-                        book_status.save()
-                        book.save()
-                        print("je l'associe à l'utilisateur courant")
-                        request.user.borrower.add(book)
+                            book_status.save()
+                            book.save()
+                            print("je l'associe à l'utilisateur courant")
+                            request.user.borrower.add(book)
 
-                        print("saved")
+                            print("saved")
 
-                    except Exception as e:
-                        print("not saved :", e)
+                        except Exception as e:
+                            print("not saved :", e)
+                    else:
+                        # save book
+                        try:
+                            book = Book.objects.create(isbn=isbn)
+                            book.isbn = i["volumeInfo"]["industryIdentifiers"][0][
+                                "identifier"
+                            ]
+                            book.title = i["volumeInfo"]["title"]
+                            book.author = i["volumeInfo"].get("authors", "...")
+                            book.cover = "../static/images/default_book_cover.jpg"
+                            book.publisher = i["volumeInfo"].get("publisher", "...")
+                            book.description = i["volumeInfo"].get("description", "...")
+                            book.category = i["volumeInfo"].get(
+                                "categories", "uncategorized"
+                            )
+                            book.page_count = i["volumeInfo"].get("pageCount", 0)
+                            book.state = "good condition"
+                            book.availability = True
+                            cured_date = dateparser.parse(i["volumeInfo"]["publishedDate"])
+                            book.published_date = cured_date.date()
+                            book_status = Ownership(
+                                book=book,
+                                customuser=request.user,
+                                state="Neuf",
+                                availability=True,
+                            )
+
+                            book_status.save()
+                            book.save()
+                            print("je l'associe à l'utilisateur courant")
+                            request.user.borrower.add(book)
+
+                            print("saved")
+
+                        except Exception as e:
+                            print("not saved :", e)
+
 
             else:
                 print("not matched")
